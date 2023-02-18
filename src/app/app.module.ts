ea733bcb517903bc2fb5b501221f9efe0560116e
Victor Oviedo <victor.oviedo.a87@gmail.com>
2023-02-18 17:01:00 -0300

    Se crearon componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { BodyComponent } from './components/home/body/body.component';
+import { FooterComponent } from './components/home/footer/footer.component';
+import { BannerComponent } from './components/home/body/banner/banner.component';
+import { SomosComponent } from './components/home/body/somos/somos.component';
+import { EspecialidadesComponent } from './components/home/body/especialidades/especialidades.component';
+import { ContactoComponent } from './components/home/body/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    BannerComponent,
+    SomosComponent,
+    EspecialidadesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
